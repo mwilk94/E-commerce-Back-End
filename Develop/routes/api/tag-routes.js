c0e29d1eece6adb9bd36977294994f55d088d112
Merkisha Wilkinson <merkisha94@gmail.com>
2022-10-17 13:13:22 -0400

    final update to routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  Tag.create(req, res)
+  Tag.create(req.body)
     .then((tag) => res.status(200).json(tag))
     .catch((err) => res.status(404).json(err));
 });
